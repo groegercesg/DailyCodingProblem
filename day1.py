648a3747146708960f90875628059eb6911f0c82
Callum <callum@computer.connect>
2020-04-23 15:13:29 +0100

    Day 1 MVP, passing the listed example

--- day1.py
+++ day1.py
@@ -1,2 +1,12 @@
-def multiply(a, b):
-    return (a*b)
+
+# Main AddToK function
+def AddToK(array, k):
+    # Outer loop for initial value
+    for i in range(0, (len(array) - 1)):
+        # Inner loop for second value
+        for j in range((i + 1), len(array)):
+            # Do two numbers from the list add up to k
+            if (array[i] + array[j] == k):
+                # If this is the case return True
+                return True
+            
--- day1_test.py
+++ day1_test.py
@@ -1,12 +1,10 @@
 import unittest
 import day1
 
-class LearningCase(unittest.TestCase):
-    def test_starting_out(self):
-        self.assertEqual(day1.multiply(3, 4), 12)
+class Day1UnitTests(unittest.TestCase):
+    def test_listed_example(self):
+        self.assertEqual(day1.AddToK([10, 15, 3, 7], 17), True, "10+7 ?= 17")
 
-def main():
-    unittest.main()
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
