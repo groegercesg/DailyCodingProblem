063b0c9a84841a163daf8074255e37398a3290d1
groegercesg <groegercesg@users.noreply.github.com>
2020-04-24 13:26:12 +0100

    Day 2 MVP, passing both listed examples

--- day2.py
+++ day2.py
@@ -1,3 +1,14 @@
 # Function
 def SpecialArrayProduct(array):
-    print("Do Stuff")
+    # prod_all_arr: value for the product of all elements of the array
+    prod_all_arr = 1
+    for value in array:
+        prod_all_arr = prod_all_arr * value
+    
+    # Recompose array
+    array_new = list()
+    for value in array:
+        array_new.append(prod_all_arr / value)
+        
+    # Output
+    return array_new
