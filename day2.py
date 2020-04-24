17276583a81147fb6126540f63a53399bd1fc231
groegercesg <groegercesg@users.noreply.github.com>
2020-04-24 13:50:04 +0100

    Day 2 Complete

--- day2.py
+++ day2.py
@@ -5,7 +5,7 @@ def SpecialArrayProduct(array):
     # prod_all_arr: value for the product of all elements of the array
     prod_all_arr = 1
     for value in array:
-        prod_all_arr = prod_all_arr * value
+        prod_all_arr *= value
     
     # Recompose array
     array_new = list()
@@ -31,7 +31,7 @@ def SpecialArrayProduct(array):
         # Find the product of this array
         prod_all_arr = 1
         for value_local in array_local:
-            prod_all_arr = prod_all_arr * value_local
+            prod_all_arr *= value_local
         
         # Append product to our output array and move onto next iteration
         array_new.append(prod_all_arr)
