05dc974ce8ff0588270306a69b4d51a19f99e19f
groegercesg <groegercesg@users.noreply.github.com>
2020-04-24 13:40:18 +0100

    Follow-up challenge met, although it is O^{2}N

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
-# Function
+'''
+# Original function MVP, this one uses division of a product of all to find our results
+# Below we have a version that don't use division!
 def SpecialArrayProduct(array):
     # prod_all_arr: value for the product of all elements of the array
     prod_all_arr = 1
@@ -11,4 +13,27 @@ def SpecialArrayProduct(array):
         array_new.append(prod_all_arr / value)
         
     # Output
+    return array_new
+'''
+
+'''
+# Follow-up: what if you can't use division?
+'''
+def SpecialArrayProduct(array):
+    # Creation of new array that is going to hold out output
+    array_new = list()
+    
+    for value in array:
+        # Create new array without value
+        array_local = array.copy()
+        array_local.remove(value)
+        
+        # Find the product of this array
+        prod_all_arr = 1
+        for value_local in array_local:
+            prod_all_arr = prod_all_arr * value_local
+        
+        # Append product to our output array and move onto next iteration
+        array_new.append(prod_all_arr)
+        
     return array_new
